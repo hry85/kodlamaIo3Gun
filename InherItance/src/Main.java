fcd5d5ae841af2991c3ad33bb390af19817805d4
Huray <shuray_erdid@hotmail.com>
2022-04-01 00:11:20 +0300

    kodlamaIo 3. gun odev1

---
+++ InherItance/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Customer customer=new Customer();
+        Employee emoloyee=new Employee();
+
+        EmployeeManager employeeManager=new EmployeeManager();
+        CustomerManager customerManager=new CustomerManager();
+
+
+    }
+}
---
+++ overriding/src/OgertmenKrediManager.java
@@ -0,0 +1,2 @@
+public class OgertmenKrediManager extends BaseKrediManager{
+}
